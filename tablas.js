b88f11d51f22475955cf350b3bbb6a369c23dcb1
Daniel Martinez <ocniel1235@gmail.com>
2025-05-30 16:57:35 -0600

    [ADD] declaré mi condición if para verificar que el valor de el usuario sea valido y si no que me muestre un mensaje en consola de que no lo es

--- tablas.js
+++ tablas.js
@@ -2,3 +2,7 @@ const prompt = require('prompt-sync')();
 
 const numero = parseInt(prompt('Introduce un número para generar su tabla de multiplicar: '));
 
+if (isNaN(numero)) {
+  console.log('Por favor, ingresa un número válido.');
+}
+
